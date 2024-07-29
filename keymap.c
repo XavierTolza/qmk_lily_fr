958e185ed1a456a26186558bbd5d54e141d0e4fe
Xavier <tolza.xavier@gmail.com>
2024-07-29 15:50:05 +0200

    wip chorded

--- keymap.c
+++ keymap.c
@@ -1,16 +1,15 @@
 #include QMK_KEYBOARD_H
-#include "keymap_french.h" 
+#include "keymap_french.h"
 #include "features/my_leds.h"
 #include "features/encoders.h"
 
-enum layer_number {
+enum layer_number
+{
     _BASE = 0,
-    _SPECIAL=1,
-    _OS = 2,
+    _SPECIAL = 1,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
     /* BASE
      * ,-----------------------------------------.                    ,-----------------------------------------.
      * | ESC  |   1! |  2@  |  3#  |  4$  |  5%  |                    |  6^  |  7&  |  8*  |  9(  |  0)  |BSpace|
@@ -21,17 +20,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|  Meh  |    | Hyper |------+------+------+------+------+------|
      * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |  ;:  |   .  |  -_  |  =+  |RCTRL |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   |LCtrl | LGUI | LAlt | / Enter /       \Space \  | RAlt | RGUI | LCtrl|
-     *                   |      |      |      |/       /         \  L1  \ |      |      |      |
+     *                   |LCtrl | LGUI | LAlt | / Enter /       \Space \  | Git  | Debug|      |
+     *                   |      |      |      |/   L1  /         \  L1  \ |(RALT)|(RCTL)|      |
      *                   `----------------------------'           '------''--------------------'
      */
     [_BASE] = LAYOUT(
-        KC_ESC, FR_AMPR, FR_EACU, FR_DQUO, FR_QUOT, FR_LPRN,                        FR_MINS, FR_EGRV, FR_UNDS, FR_CCED, FR_AGRV, KC_BSPC,
-        KC_TAB, FR_A, FR_Z, FR_E, FR_R, FR_T,                                       FR_Y, FR_U, FR_I, FR_O, FR_P, KC_DEL,
-        LSFT_T(FR_LPRN), FR_Q, FR_S, FR_D, FR_F, FR_G,                              FR_H, FR_J, FR_K, FR_L, FR_M, LSFT_T(FR_RPRN),
-        LCTL_T(OSM(MOD_MEH)), FR_W, FR_X, FR_C, FR_V, FR_B, _______,                LALT(KC_F4), FR_N, FR_COMM, FR_SCLN, FR_COLN, KC_SLSH,KC_LCTL,
-                KC_LCTL, LT(_OS,KC_LGUI), KC_LALT, LT(_SPECIAL, KC_ENT),                    LT(_SPECIAL, KC_SPACE), KC_RALT, KC_RGUI, KC_LCTL
-        ),
+        KC_ESC, FR_AMPR, FR_EACU, FR_DQUO, FR_QUOT, FR_LPRN, /**/ FR_MINS, FR_EGRV, FR_UNDS, FR_CCED, FR_AGRV, KC_BSPC,
+        KC_TAB, FR_A, FR_Z, FR_E, FR_R, FR_T, /**/ FR_Y, FR_U, FR_I, FR_O, FR_P, KC_DEL,
+        LSFT_T(FR_LPRN), FR_Q, FR_S, FR_D, FR_F, FR_G, /**/ FR_H, FR_J, FR_K, FR_L, FR_M, LSFT_T(FR_RPRN),
+        LCTL_T(OSM(MOD_MEH)), FR_W, FR_X, FR_C, FR_V, FR_B, _______, /**/ LALT(KC_F4), FR_N, FR_COMM, FR_SCLN, FR_COLN, KC_SLSH, KC_LCTL,
+        KC_LCTL, LT(_OS, KC_LGUI), KC_LALT, LT(_SPECIAL, KC_ENT), LT(_SPECIAL, KC_SPACE), KC_RALT, KC_RCTL, KC_NONE),
 
     /* SPECIAL
      * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -48,106 +46,101 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   `----------------------------'           '------''--------------------'
      */
     [_SPECIAL] = LAYOUT(
-        RCS(FR_E), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, TG(2),
-        RCS(FR_F), FR_TILD, FR_SLSH, FR_LCBR, FR_RCBR, FR_PERC,                           FR_EQL, _______, KC_UP, FR_QUOT, FR_DQUO, FR_GRV,
-        RCS(FR_G), FR_EURO, FR_BSLS, FR_LPRN, FR_RPRN, FR_AT,                              KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,
-        RCS(FR_D), RCS(FR_D), FR_DLR, FR_PIPE, FR_LBRC, FR_RBRC, FR_HASH, KC_PGDN,         KC_PGUP, FR_MINS, FR_PLUS, FR_EQL, FR_ASTR, FR_EXLM, _______,
-                _______, FR_SUP2, FR_RABK, _______,                                     _______, _______, _______, _______
-    ),
-    /* OS
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * | ESC  |   1! |  2@  |  3#  |  4$  |  5%  |                    |  6^  |  7&  |  8*  |  9(  |  0)  |BSpace|
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | Tab  |   A  |   Z  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | DEL  |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |LShift|   Q  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   M  | altf4|
-     * |------+------+------+------+------+------|  Meh  |    | Hyper |------+------+------+------+------+------|
-     * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |  ;:  |   .  |  -_  |  =+  |RCTRL |
-     * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   |LCtrl | LGUI | LAlt | / Enter /       \Space \  | RAlt | RGUI | LCtrl|
-     *                   |      |      |      |/       /         \  L1  \ |      |      |      |
-     *                   `----------------------------'           '------''--------------------'
-     */
-    [_OS] = LAYOUT(
-        RGB_TOG, FR_AMPR, FR_EACU, FR_DQUO, FR_QUOT, FR_LPRN,                        FR_MINS, FR_EGRV, FR_UNDS, DM_PLY1, DM_PLY2, DM_RSTP,
-        KC_TAB, FR_A, FR_Z, FR_E, FR_R, MEH(FR_T),                                       FR_Y, FR_U, FR_I, DM_REC1, DM_REC2, KC_DEL,
-        LSFT_T(FR_LPRN), FR_Q, FR_S, FR_D, KC_WHOM, FR_G,                              FR_H, FR_J, FR_K, FR_L, FR_M, LSFT_T(FR_RPRN),
-        LCTL_T(OSM(MOD_MEH)), FR_W, FR_X, FR_C, FR_V, FR_B, KC_MPLY,                LALT(KC_F4), FR_N, FR_COMM, FR_SCLN, FR_COLN, KC_SLSH,KC_LCTL,
-                KC_LCTL, LT(_OS,KC_LGUI), KC_LALT, LT(_SPECIAL, KC_ENT),                    LT(_SPECIAL, KC_SPACE), KC_RALT, KC_RGUI, KC_LCTL
-        )
-    };
+        RCS(FR_E), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, TG(2),
+        RCS(FR_F), FR_TILD, FR_SLSH, FR_LCBR, FR_RCBR, FR_PERC, FR_EQL, _______, KC_UP, FR_QUOT, FR_DQUO, FR_GRV,
+        RCS(FR_G), FR_EURO, FR_BSLS, FR_LPRN, FR_RPRN, FR_AT, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,
+        RCS(FR_D), RCS(FR_D), FR_DLR, FR_PIPE, FR_LBRC, FR_RBRC, FR_HASH, KC_PGDN, KC_PGUP, FR_MINS, FR_PLUS, FR_EQL, FR_ASTR, FR_EXLM, _______,
+        _______, FR_SUP2, FR_RABK, _______, _______, _______, _______, _______)};
 
 #if defined(ENCODER_ENABLE)
-const encoder_config_t encoder_layers[NB_LAYERS][NUM_ENCODERS][ENCODER_NB_CONFIGS]={
-    {{{ALL_MODS,LCTL(FR_Y),LCTL(FR_Z)}},{{ALL_MODS, KC_WH_D,KC_WH_U}}},
-    {{{ALL_MODS,LCTL(FR_Y),LCTL(FR_Z)}},{{ALL_MODS, KC_PGDN,KC_PGUP}}},
-    {{{ALL_MODS,KC_VOLU,KC_VOLD}},{{ALL_MODS,KC_VOLU,KC_VOLD}}},
+const encoder_config_t encoder_layers[NB_LAYERS][NUM_ENCODERS][ENCODER_NB_CONFIGS] = {
+    {{{ALL_MODS, LCTL(FR_Y), LCTL(FR_Z)}}, {{ALL_MODS, KC_WH_D, KC_WH_U}}},
+    {{{ALL_MODS, LCTL(FR_Y), LCTL(FR_Z)}}, {{ALL_MODS, KC_PGDN, KC_PGUP}}},
+    {{{ALL_MODS, KC_VOLU, KC_VOLD}}, {{ALL_MODS, KC_VOLU, KC_VOLD}}},
 };
 #endif // defined(ENCODER_ENABLE)
 
 #ifdef CUSTOM_RGB_LAYERS
-    /* RGB layout
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |   0  |   2  |   3  |   4  |   5  |   6  |                    |  F6  |  F7  |  36  | F9   | F10  | TG(2)|
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |   ~  |   /  |  9   |  10  |   %  |                    |      |      |  42  |   '  |   "  |   `  |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |   €  |   \  |  15  |  16  |   @  |-------.    ,-------| Home |  47  | Down |Right | End  |      |
-     * |------+------+------+------+------+------| PgDo  |    | PgUp  |------+------+------+------+------+------|
-     * |      |   $  |   |  |  21  |  22  |   #  |-------|    |-------|   -  |   +  |   =  |   *  |   !  |      |
-     * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   |      |   <  |   >  | /       /       \      \  |      |      |      |
-     *                   |      |      |      |/       /         \      \ |      |      |      |
-     *                   `----------------------------'           '------''--------------------'
-     */
-const rgb_layout_t rgb_layout[NB_RGB_LAYOUTS]={
-    {0,ALL_MODS,RGB_GREEN,1,{0}},
+/* RGB layout
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |   0  |   2  |   3  |   4  |   5  |   6  |                    |  F6  |  F7  |  36  | F9   | F10  | TG(2)|
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   ~  |   /  |  9   |  10  |   %  |                    |      |      |  42  |   '  |   "  |   `  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   €  |   \  |  15  |  16  |   @  |-------.    ,-------| Home |  47  | Down |Right | End  |      |
+ * |------+------+------+------+------+------| PgDo  |    | PgUp  |------+------+------+------+------+------|
+ * |      |   $  |   |  |  21  |  22  |   #  |-------|    |-------|   -  |   +  |   =  |   *  |   !  |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |   <  |   >  | /       /       \      \  |      |      |      |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+const rgb_layout_t rgb_layout[NB_RGB_LAYOUTS] = {
+    {0, ALL_MODS, RGB_GREEN, 1, {0}},
     // Parenthesis in special layer
-    {2,ALL_MODS,RGB_GREEN,6,{9,10,15,16,21,22}},
+    {2, ALL_MODS, RGB_GREEN, 6, {9, 10, 15, 16, 21, 22}},
     // Directionnal arrows
-    {2,ALL_MODS,RGB_ORANGE,4,{42,47,48,49}},
+    {2, ALL_MODS, RGB_ORANGE, 4, {42, 47, 48, 49}},
     // Home end
-    {2,ALL_MODS,RGB_RED,2,{46,50}},
+    {2, ALL_MODS, RGB_RED, 2, {46, 50}},
     // Quotes
     //{2,ALL_MODS,RGB_BLUE,3,{43,44,45}},
     // Math
-    {2,ALL_MODS,221*0.1, 88*0.1, 214*0.1,5,{53,54,55,56,57}},
+    {2, ALL_MODS, 221 * 0.1, 88 * 0.1, 214 * 0.1, 5, {53, 54, 55, 56, 57}},
 };
 #endif
 
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case LT(_SPECIAL, KC_SPACE):
-        case LT(_SPECIAL, KC_ENT):
-            return 200;
-        
-        default:
-            return TAPPING_TERM;
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record)
+{
+    switch (keycode)
+    {
+    case LT(_SPECIAL, KC_SPACE):
+    case LT(_SPECIAL, KC_ENT):
+        return 200;
+
+    default:
+        return TAPPING_TERM;
     }
 }
 
 // Turn off Liatris LED
-void keyboard_pre_init_user(void) {
-  // Set our LED pin as output
-  setPinOutput(24);
-  // Turn the LED off
-  // (Due to technical reasons, high is off and low is on)
-  writePinHigh(24);
+void keyboard_pre_init_user(void)
+{
+    // Set our LED pin as output
+    setPinOutput(24);
+    // Turn the LED off
+    // (Due to technical reasons, high is off and low is on)
+    writePinHigh(24);
 }
 
-void keyboard_post_init_user(void) {
+void keyboard_post_init_user(void)
+{
     reset_leds();
 }
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    #ifdef CUSTOM_RGB_LAYERS
+layer_state_t layer_state_set_user(layer_state_t state)
+{
+#ifdef CUSTOM_RGB_LAYERS
     rgb_handle_layer_callback(state);
-    #endif
+#endif
     return state;
 }
 
-
-
-
-
-
+// Process chorded key shortcuts
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+    if (record->event.pressed)
+    {
+        if (get_mods() == MOD_BIT(KC_RALT))
+        {
+            // Vscode git
+            tap_code(LAlt(FR_G));
+        }
+        else if (get_mods() == MOD_BIT(KC_RCTL))
+        {
+            // Vscode debug
+            tap_code(LAlt(FR_D));
+        }
+    }
+    return true;
+};
